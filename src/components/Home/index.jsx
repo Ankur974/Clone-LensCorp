dbbf923e446f65a08c1aec9b5f5c4fb408f0cb2b
Ankur974 <ankurpandey974@gmail.com>
2024-05-30 11:39:16 +0530

    fix:ui

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Nav from "../Nav";
+import SecondContainer from "./SecondContainer";
+import HeaderSection from "./HeadderSection";
+import CardContainer from "./ServicesCard";
+import List from "./List";
+import Vision from "./Vision";
+import Blog from "./Blog";
+import Faq from "./Faq";
+import Footer from "../common/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <SecondContainer />
+      <HeaderSection />
+      <CardContainer />
+      <List />
+      <Vision />
+      <Blog />
+      <Faq />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
